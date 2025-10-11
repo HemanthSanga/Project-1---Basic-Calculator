9d83c878060f6a477f00be0987689713be3b53df
HemanthSanga <sanga.hemanth1995@gmail.com>
2025-10-11 18:19:17 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,23 @@
+  const display = document.getElementById("display");
+
+  function append(value) {
+    if (display.innerText === "0") display.innerText = value;
+    else display.innerText += value;
+  }
+
+  function clearDisplay() {
+    display.innerText = "0";
+  }
+
+  function deleteChar() {
+    display.innerText = display.innerText.slice(0, -1) || "0";
+  }
+
+  function calculate() {
+    try {
+      display.innerText = eval(display.innerText.replace('÷', '/').replace('×', '*'));
+    } catch {
+      display.innerText = "Error";
+      setTimeout(clearDisplay, 1500);
+    }
+  }
